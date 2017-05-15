465c1d3297f250cc4a8a3faa665cb6adb9a8384f
Ryan Wirth <ryan@ryanwirth.ca>
2017-05-14 17:48:26 -0700

    Complete login script

---
+++ server/login.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Outputs the given array as a JSON object and kills the script.
+ */
+function outputResponse($data) {
+    header('Access-Control-Allow-Origin: *');  
+    header("Content-type: text/json");
+    
+    echo json_encode($data);
+    exit;
+}
+
+/** HANDLE LOGINS **/
+
+// First, get the email and password
+$email = $_GET['email'];
+$password = $_GET['password'];
+
+// Validate the email format
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    outputResponse(array("status" => "error", "type" => "invalid_email"));
+}
+
+// Clean the password
+$password = trim($password);
+if($password == '') {
+    outputResponse(array("status" => "error", "type" => "invalid_password"));
+}
+
+// Hash the password
+$password_hashed =  crypt($password, $salt);
+
+// Connect to mysql
+include("configuration.php");
+
+// Submit the query
+$stmt = $conn->prepare("SELECT user_verified, user_email, user_displayname, user_description, user_logohash, user_url, user_tags FROM ubcollaborate_users WHERE user_email=? AND user_password=? LIMIT 1");
+$stmt->bind_param('ss', $email, $password_hashed);
+$stmt->execute();
+$stmt->bind_result($user_verified, $user_email, $user_displayname, $user_description, $user_logohash, $user_url, $user_tags);
+
+while ($stmt->fetch())
+{
+    // If the user isn't verified yet, return an error
+    $user_verified = boolval($user_verified);
+    if(!$user_verified) outputResponse(array("status" => "error", "type" => "user_not_verified"));
+    
+    // The user is verified, output their data
+    outputResponse(array("status" => "success",
+                         "user_email" => $user_email,
+                         "user_displayname" => $user_displayname,
+                         "user_description" => $user_description,
+                         "user_logohash"    => $user_logohash,
+                         "user_url"         => $user_url,
+                         "user_tags"        => $user_tags));
+}
+
+// If we get here, no rows were returned
+outputResponse(array("status" => "error", "type" => "invalid_password"));
+
+?>
